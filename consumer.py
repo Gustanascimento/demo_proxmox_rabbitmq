067db00cc2a3b89b62a3fc5fd7ba7af4a96c1309
Lucas Rodrigues <lucas.darlindo@gmail.com>
2024-10-26 00:13:05 -0300

    enhance: minor code structure updates Co-authored-by: Gustavo Nascimento <56653979+gustanascimento@users.noreply.github.com>

--- consumer.py
+++ consumer.py
@@ -1,14 +1,20 @@
-import pika
-from time import sleep
 from datetime import datetime
+from time import sleep
+
+import pika
+
 
 class RabbitMQConsumer:
     def __init__(self, queue_name: str):
         self.queue_name = queue_name
         self.host = "192.168.100.23"
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.host)
+        )
         self.channel = self.connection.channel()
+
         self.channel.queue_declare(queue=self.queue_name)
+        self.channel.basic_qos(prefetch_count=1)
 
     def run_consumer(self):
         print(f'📡 Aguardando mensagens na fila "{self.queue_name}"...\n')
@@ -18,6 +24,7 @@ class RabbitMQConsumer:
             print(f" ✅ Mensagem processada às {datetime.now().strftime('%H:%M:%S')}: {message}")
             self.channel.basic_ack(method.delivery_tag)
             sleep(4)
+
 
 if __name__ == "__main__":
     consumer = RabbitMQConsumer(queue_name='test_queue')
--- producer.py
+++ producer.py
@@ -1,12 +1,17 @@
 import pika
 
+
 class RabbitMQProducer:
     def __init__(self, queue_name):
         self.host = "192.168.100.23"
         self.queue_name = queue_name
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.host)
+        )
         self.channel = self.connection.channel()
+
         self.channel.queue_declare(queue=self.queue_name)
+        self.channel.basic_qos(prefetch_count=1)
 
     def send_message(self, message):
         self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message)
@@ -14,6 +19,7 @@ class RabbitMQProducer:
 
     def close_connection(self):
         self.connection.close()
+
 
 if __name__ == "__main__":
     queue_name = 'test_queue'
